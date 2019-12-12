5c80340879d6284d28494f3a4113425c0e3a65e0
Mantas Zakas <mantas.zakas@gmail.com>
2019-12-12 14:08:52 +0200

    Header style done

--- wp-content/themes/andrum/templates/header.php
+++ wp-content/themes/andrum/templates/header.php
@@ -1,12 +1,19 @@
 <header class="banner">
-  <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav class="nav-primary">
-      <?php
-      if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-      endif;
-      ?>
-    </nav>
-  </div>
+	<div class="container d-flex justify-content-end header__top">
+		<p>+370 527 27092</p>
+		<div class="header__dash my-auto"></div>
+		<p>pardavimai@andrum.lt</p>
+	</div>
+	<div class="header__nav d-flex align-items-center">
+		<div class="container h-100">
+			<nav class="navbar navbar-expand-sm p-0 justify-content-between h-100">
+				<a class="navbar-brand" href="#">Logo</a>
+				<ul class="navbar-nav h-100 p-0">
+					<li class="nav-item d-flex"><a class="nav-link align-self-stretch py-0 header__link" href="#">Link 1</a></li>
+					<li class="nav-item d-flex"><a class="nav-link align-self-stretch py-0 header__link" href="#">Link 2</a></li>
+					<li class="nav-item d-flex"><a class="nav-link align-self-stretch py-0 header__link" href="#">Link 3</a></li>
+				</ul>
+			</nav>
+		</div>
+	</div>
 </header>
