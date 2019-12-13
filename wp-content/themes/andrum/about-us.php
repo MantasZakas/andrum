de7764413e7066bff11dc423654a867c44710e42
Mantas Zakas <mantas.zakas@gmail.com>
2019-12-13 14:53:05 +0200

    About us layout done

---
+++ wp-content/themes/andrum/about-us.php
@@ -0,0 +1,73 @@
+<?php
+/*
+ * Template Name: About us
+ */
+?>
+
+<h1 class="page__title--main text-center">Apie mus</h1>
+<h2 class="page__title--name text-center">Valdyba</h2>
+<div class="row d-flex justify-content-center">
+	<div class="col-md-4 page__card">
+		<div class="fakeimage"></div>
+		<div class="page__description">
+			<div class="page__description--top">
+				<p class="page__description--text1 text-center">Specialistės gebėjimai
+				organizuoti ir užtikrinti sėkmingas įmonės išorinės ir vidinės
+				komunikacijos ir rinkodaros strategijas pagrįsti 20 metų darbo
+				patirtimi įvairiuose komunikacijos kanaluose. Sukauptas žinių
+				potencialas bei asmeninės savybės skatina generuoti ir realizuoti
+				novatoriškas, ekonomiškai pagrįstas idėjas.</p>
+				<p class="page__description--text2 text-center">„Efektyvi komunikacija didina bendrovės vertę. Visi verslai ir veiklos 
+				prasideda tik su visuomenės leidimu ir egzistuoja su jos palaikymu.​“</p>
+				</div>
+			<div class="page__description--bottom">
+				<h2 class="page__title--name text-center">Mindaugas Ruseckas</h2>
+				<h3 class="page__title--position text-center">Pardavimų ir NT valdymas</h3>
+			</div>
+		</div>
+	</div>
+	<div class="col-md-4 page__card">
+		<div class="fakeimage"></div>
+		<div class="page__description">
+			<div class="page__description--top"></div>
+			<div class="page__description--bottom">
+				<h2 class="page__title--name text-center">Mindaugas Ruseckas</h2>
+				<h3 class="page__title--position text-center">Pardavimų ir NT
+					valdymas</h3>
+			</div>
+		</div>
+	</div>
+	<div class="col-md-4 page__card">
+		<div class="fakeimage"></div>
+		<div class="page__description">
+			<div class="page__description--top"></div>
+			<div class="page__description--bottom">
+				<h2 class="page__title--name text-center">Mindaugas Ruseckas</h2>
+				<h3 class="page__title--position text-center">Pardavimų ir NT
+					valdymas</h3>
+			</div>
+		</div>
+	</div>
+	<div class="col-md-4 page__card">
+		<div class="fakeimage"></div>
+		<div class="page__description">
+			<div class="page__description--top"></div>
+			<div class="page__description--bottom">
+				<h2 class="page__title--name text-center">Mindaugas Ruseckas</h2>
+				<h3 class="page__title--position text-center">Pardavimų ir NT
+					valdymas</h3>
+			</div>
+		</div>
+	</div>
+	<div class="col-md-4 page__card">
+		<div class="fakeimage"></div>
+		<div class="page__description">
+			<div class="page__description--top"></div>
+			<div class="page__description--bottom">
+				<h2 class="page__title--name text-center">Mindaugas Ruseckas</h2>
+				<h3 class="page__title--position text-center">Pardavimų ir NT
+					valdymas</h3>
+			</div>
+		</div>
+	</div>
+</div>
