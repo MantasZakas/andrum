875ab495061d5bc6d05104b258d80f64379807a4
Mantas Zakas <mantas.zakas@gmail.com>
2019-12-13 11:16:00 +0200

    Footer layout done

--- wp-content/themes/andrum/templates/footer.php
+++ wp-content/themes/andrum/templates/footer.php
@@ -1,5 +1,23 @@
 <footer class="content-info">
-  <div class="container">
-    <?php dynamic_sidebar('sidebar-footer'); ?>
+  <div class="footer__container">
+  	<div class="container">
+  		<div class="row">
+  			<div class="col-md-3">
+  				<h3 class="footer__heading">Apie mus</h3>
+  			</div>
+  			<div class="col-md-3">
+  				<h3 class="footer__heading">Plėtojami projektai</h3>
+  			</div>
+  			<div class="col-md-3">
+  				<h3 class="footer__heading">Naujienos</h3>
+  			</div>
+  			<div class="col-md-3">
+  				<h3 class="footer__heading">Kontaktai</h3>
+  			</div>
+  		</div>
+  		<div class="row d-flex justify-content-center">
+  			<p>Sprendimas:</p>
+  		</div>
+  	</div>
   </div>
 </footer>
