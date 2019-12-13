3de148d38ef3da50e935c828b0ee2b3a7d05f524
Mantas Zakas <mantas.zakas@gmail.com>
2019-12-13 17:44:36 +0200

    Main nav menu now customisable

--- wp-content/themes/andrum/functions.php
+++ wp-content/themes/andrum/functions.php
@@ -9,20 +9,41 @@
  *
  * @link https://github.com/roots/sage/pull/1042
  */
-$sage_includes = [
-  'lib/assets.php',    // Scripts and stylesheets
-  'lib/extras.php',    // Custom functions
-  'lib/setup.php',     // Theme setup
-  'lib/titles.php',    // Page titles
-  'lib/wrapper.php',   // Theme wrapper class
-  'lib/customizer.php' // Theme customizer
+$sage_includes = [ 
+		'lib/assets.php', // Scripts and stylesheets
+		'lib/extras.php', // Custom functions
+		'lib/setup.php', // Theme setup
+		'lib/titles.php', // Page titles
+		'lib/wrapper.php', // Theme wrapper class
+		'lib/customizer.php' // Theme customizer
 ];
 
-foreach ($sage_includes as $file) {
-  if (!$filepath = locate_template($file)) {
-    trigger_error(sprintf(__('Error locating %s for inclusion', 'sage'), $file), E_USER_ERROR);
-  }
+foreach ( $sage_includes as $file ) {
+	if (! $filepath = locate_template ( $file )) {
+		trigger_error ( sprintf ( __ ( 'Error locating %s for inclusion', 'sage' ), $file ), E_USER_ERROR );
+	}
 
-  require_once $filepath;
+	require_once $filepath;
 }
-unset($file, $filepath);
+unset ( $file, $filepath );
+
+//Register the main nav menu
+add_action ( 'init', function () {
+	register_nav_menus ( [ 
+			'top_menu' => 'Top menu'
+	] );
+} );
+
+//add custom classes to menu <li> and <a> tags
+add_filter ( 'nav_menu_css_class', function ($classes, $item) {
+	$classes [] = 'nav-item d-flex';
+	if (in_array('current-menu-item', $classes) ){
+		$classes[] = 'header__link--active';
+	}
+	return $classes;
+}, 10, 2 );
+
+add_filter ( 'nav_menu_link_attributes', function ($atts) {
+	$atts ['class'] = "nav-link align-self-stretch py-0 header__link";
+	return $atts;
+}, 100, 1 );
--- wp-content/themes/andrum/templates/header.php
+++ wp-content/themes/andrum/templates/header.php
@@ -7,13 +7,22 @@
 	<div class="header__nav d-flex align-items-center">
 		<div class="container h-100">
 			<nav class="navbar navbar-expand-sm p-0 justify-content-between h-100">
-				<a class="navbar-brand" href="#">Logo</a>
-				<ul class="navbar-nav h-100 p-0">
-					<li class="nav-item d-flex"><a class="nav-link align-self-stretch py-0 header__link" href="#">Link 1</a></li>
-					<li class="nav-item d-flex"><a class="nav-link align-self-stretch py-0 header__link" href="#">Link 2</a></li>
-					<li class="nav-item d-flex"><a class="nav-link align-self-stretch py-0 header__link" href="#">Link 3</a></li>
-				</ul>
+				<a class="navbar-brand" href="#"><img class="header__logo" alt="Site Logo" src="<?php echo get_template_directory_uri()?>/img/logo-full.svg"></a>
+				<?php wp_nav_menu([
+					'menu' => 'top_menu',
+					'container' => '',
+					'items_wrap' => '<ul id="%1$s" class="navbar-nav h-100 p-0">%3$s</ul>',
+					'menu_class' => 'nav-item d-flex'
+				])?>
+<!-- 				<ul class="navbar-nav h-100 p-0"> -->
+<!-- 					<li class="nav-item d-flex"><a -->
+<!-- 						class="nav-link align-self-stretch py-0 header__link" href="#">Link -->
+<!-- 							1</a></li> -->
+<!-- 					<li class="nav-item d-flex"><a class="nav-link align-self-stretch py-0 header__link" href="#">Link 2</a></li> -->
+<!-- 					<li class="nav-item d-flex"><a class="nav-link align-self-stretch py-0 header__link" href="#">Link 3</a></li> -->
+<!-- 				</ul> -->
 			</nav>
 		</div>
 	</div>
 </header>
+
