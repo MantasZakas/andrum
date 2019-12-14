363c213717858d5660e1e059b8d2f31081b7a143
Mantas Zakas <mantas.zakas@gmail.com>
2019-12-14 18:15:52 +0200

    News card in homepage now customisable
    Also uploaded some images for posts

--- wp-content/themes/andrum/index.php
+++ wp-content/themes/andrum/index.php
@@ -1,16 +1,37 @@
-<?php get_template_part('templates/page', 'header'); ?>
+<h1 class="page__title--homepage text-center">Šiuo metu vystomi projektai</h1>
 
-<?php if (!have_posts()) : ?>
-  <div class="alert alert-warning">
-    <?php _e('Sorry, no results were found.', 'sage'); ?>
-  </div>
-  <?php get_search_form(); ?>
-<?php endif; ?>
 
-<?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/content', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
-<?php endwhile; ?>
 
-<?php the_posts_navigation(); ?>
+<h2 class="page__title--homepage text-center">Įgyvendinti projektai</h2>
 
-<?php get_template_part('templates/page', 'footer'); ?>
+
+
+<h2 class="page__title--homepage text-center">Naujienos</h2>
+<div class="row">
+	<?php
+	$news = get_posts ( [ 
+			'numberposts' => 3,
+			'category_name' => 'news'
+	] );
+	foreach ( $news as $n ) {
+	?>
+	<div class="col-md-4">
+		<div class="newsCard__wrapper h-100">
+			<img class="newsCard__image" src="<?= get_the_post_thumbnail_url($n) ?>">
+			<h3 class="newsCard__title"><?= get_the_title($n) ?></h3>
+			<p class="newsCard__excerpt"><?= get_the_excerpt($n) ?></p>
+		</div>
+	</div>
+	
+	<?php } ?>
+</div>
+<div class="d-flex justify-content-center">
+	<a class="newsButton" href="<?= get_permalink(get_page_by_path('naujienos')) ?>">Visos naujienos</a>
+</div>
+
+
+
+
+
+
+
