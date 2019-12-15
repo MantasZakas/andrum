56456eea1379246e3494bfa596dbd2e1696f263c
Mantas Zakas <mantas.zakas@gmail.com>
2019-12-15 12:27:54 +0200

    Finished projects section in homepage now customisable
    Also fixed a minor issue with page width

--- wp-content/themes/andrum/index.php
+++ wp-content/themes/andrum/index.php
@@ -1,37 +1,48 @@
+<!-- Current project banner -->
 <h1 class="page__title--homepage text-center">Šiuo metu vystomi projektai</h1>
 
 
-
+<!-- Finished projects -->
 <h2 class="page__title--homepage text-center">Įgyvendinti projektai</h2>
 
-
-
-<h2 class="page__title--homepage text-center">Naujienos</h2>
 <div class="row">
 	<?php
-	$news = get_posts ( [ 
-			'numberposts' => 3,
-			'category_name' => 'news'
+	$projects = get_posts ( [ 
+			'numberposts' => 4,
+			'category_name' => 'finished-projects'
 	] );
-	foreach ( $news as $n ) {
+	foreach ( $projects as $p ) {
 	?>
-	<div class="col-md-4">
-		<div class="newsCard__wrapper h-100">
-			<img class="newsCard__image" src="<?= get_the_post_thumbnail_url($n) ?>">
-			<h3 class="newsCard__title"><?= get_the_title($n) ?></h3>
-			<p class="newsCard__excerpt"><?= get_the_excerpt($n) ?></p>
+	<div class="col-md-6 projectCard__wrapper">
+		<img class="projectCard__image" src="<?= get_the_post_thumbnail_url($p) ?>">
+		<div class="projectCard__text">
+			<h3 class="projectCard__text--title"><?= get_the_title($p) ?></h3>
+			<p class="projectCard__text--excerpt"><?= get_the_excerpt($p) ?></p>
 		</div>
 	</div>
-	
 	<?php } ?>
 </div>
-<div class="d-flex justify-content-center">
-	<a class="newsButton" href="<?= get_permalink(get_page_by_path('naujienos')) ?>">Visos naujienos</a>
+<!-- News -->
+<h2 class="page__title--homepage text-center">Naujienos</h2>
+<div class="container">
+	<div class="row">
+		<?php
+		$news = get_posts ( [ 
+				'numberposts' => 3,
+				'category_name' => 'news'
+		] );
+		foreach ( $news as $n ) {
+		?>
+		<div class="col-md-4">
+			<div class="newsCard__wrapper h-100">
+				<img class="newsCard__image" src="<?= get_the_post_thumbnail_url($n) ?>">
+				<h3 class="newsCard__title"><?= get_the_title($n) ?></h3>
+				<p class="newsCard__excerpt"><?= get_the_excerpt($n) ?></p>
+			</div>
+		</div>
+		<?php } ?>
+	</div>
+	<div class="d-flex justify-content-center">
+		<a class="newsButton" href="<?= get_permalink(get_page_by_path('naujienos')) ?>">Visos naujienos</a>
+	</div>
 </div>
-
-
-
-
-
-
-
