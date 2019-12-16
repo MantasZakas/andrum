db62ef37894f7520af46f2e071a872ee98308ff3
Mantas Zakas <mantas.zakas@gmail.com>
2019-12-16 13:43:41 +0200

    Added links to relevant items in the index page and footer

--- wp-content/themes/andrum/index.php
+++ wp-content/themes/andrum/index.php
@@ -10,7 +10,11 @@
 		?>
 		<img class="backgroundImage" src="<?= get_the_post_thumbnail_url($currentProject[0]) ?>">
 		<div class="banner__text whiteText">
-			<h3 class="banner__text--title"><?= get_the_title($currentProject[0]) ?></h3>
+			<h3 class="banner__text--title">
+				<a class="whiteLink" href="<?= get_the_guid($currentProject[0]) ?>">
+					<?= get_the_title($currentProject[0]) ?>
+				</a>
+			</h3>
 			<p class="banner__text--excerpt"><?= get_the_excerpt($currentProject[0]) ?></p>
 		</div>
 	</div>
@@ -28,8 +32,14 @@
 	<div class="col-md-6 projectCard__wrapper">
 		<img class="projectCard__image" src="<?= get_the_post_thumbnail_url($p) ?>">
 		<div class="projectCard__text whiteText">
-			<h3 class="projectCard__text--title"><?= get_the_title($p) ?></h3>
-			<p class="projectCard__text--excerpt"><?= get_the_excerpt($p) ?></p>
+			<h3 class="projectCard__text--title">
+				<a class="whiteLink" href="<?= get_the_guid($p) ?>">
+					<?= get_the_title($p) ?>
+				</a>
+			</h3>
+			<p class="projectCard__text--excerpt">
+				<?= get_the_excerpt($p) ?>
+			</p>
 		</div>
 	</div>
 	<?php } ?>
@@ -48,7 +58,11 @@
 		<div class="col-md-4">
 			<div class="newsCard__wrapper h-100">
 				<img class="newsCard__image" src="<?= get_the_post_thumbnail_url($n) ?>">
-				<h3 class="newsCard__title"><?= get_the_title($n) ?></h3>
+				<h3 class="newsCard__title">
+					<a class="newsCard__title--link" href="<?= get_the_guid($n) ?>">
+						<?= get_the_title($n) ?>
+					</a>
+				</h3>
 				<p class="newsCard__excerpt"><?= get_the_excerpt($n) ?></p>
 			</div>
 		</div>
--- wp-content/themes/andrum/templates/footer.php
+++ wp-content/themes/andrum/templates/footer.php
@@ -27,10 +27,16 @@ define('CONTACTS_ID', 63);
 				?>
 				<div class="row footer__card">
 					<div class="col-4">
-						<img class="footer__image" src="<?= get_the_post_thumbnail_url($p) ?>">
+						<a href="<?= get_the_guid($p) ?>">
+							<img class="footer__image" src="<?= get_the_post_thumbnail_url($p) ?>">
+						</a>
 					</div>				
 					<div class="col-8 footer__text">
-						<h3 class="footer__heading"><?= get_the_title($p) ?></h3>
+						<h3 class="footer__heading">
+							<a href="<?= get_the_guid($p) ?>" class="footer__heading--link">
+								<?= get_the_title($p) ?>
+							</a>
+						</h3>
 						<p class="mb-0"><?= get_the_excerpt($p) ?></p>
 					</div>
 				</div>
@@ -48,7 +54,9 @@ define('CONTACTS_ID', 63);
 				?>
 				<div class="row footer__card">
 					<div class="col-4">
-						<img class="footer__image" src="<?= get_the_post_thumbnail_url($n) ?>">
+						<a href="<?= get_the_guid($n) ?>">
+							<img class="footer__image" src="<?= get_the_post_thumbnail_url($n) ?>">
+						</a>
 					</div>				
 					<div class="col-8 footer__text d-flex align-items-center">
 						<p class="mb-0"><?= get_the_title($n) ?></p>
@@ -74,15 +82,18 @@ define('CONTACTS_ID', 63);
   		<div class="row">
   			<div class="col-md-3"></div>
   			<div class="col-md-3 d-flex justify-content-center">
-  				<a class="footer__link">Visi projektai</a>
+  				<a href="<?= get_home_url() ?>" class="footer__link">Visi projektai</a>
   			</div>
   			<div class="col-md-3 d-flex justify-content-center">
-  				<a class="footer__link">Visos naujienos</a>
+  				<a href="<?= get_permalink(get_page_by_path('naujienos')) ?>" class="footer__link">Visos naujienos</a>
   			</div>
   			<div class="col-md-3"></div>
   		</div>
   		<div class="row d-flex justify-content-center footer__madeBy">
-  			<a class="footer__link">Sprendimas:<img class="footer__madeBy--logo" alt="Western Investment" src="<?= get_template_directory_uri()?>/img/wi-logo.svg"></a>
+  			<a href="https://w-i.lt/" class="footer__link footer__link--wi">
+  				Sprendimas:
+  				<img class="footer__madeBy--logo" alt="Western Investment" src="<?= get_template_directory_uri()?>/img/wi-logo.svg">
+  			</a>
   		</div>
   	</div>
   </div>
